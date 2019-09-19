c9e287cd81a2a4066d879f07134ef156a400ee24
Luis Fernando Oliveira <luisfmoraes.r@gmail.com>
2019-09-19 11:57:24 -0300

    finish project

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,31 @@
 import { Router } from 'express';
+import axios from 'axios';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ ok: true });
+routes.get('/api/users', async (req, res) => {
+  const { since } = req.query;
+  const response = await axios.get(`https://api.github.com/users`, {
+    params: {
+      since,
+    },
+  });
+  res.json(response.data);
+});
+
+routes.get('/api/users/:username/details', async (req, res) => {
+  const { username } = req.params;
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+
+  res.json(response.data);
+});
+
+routes.get(`/api/users/:username/repos`, async (req, res) => {
+  const { username } = req.params;
+  const response = await axios.get(
+    `https://api.github.com/users/${username}/repos`
+  );
+  res.json(response.data);
 });
 
 export default routes;
